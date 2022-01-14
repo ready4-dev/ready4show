f71119a04eb081b9f64a2b56c41bc8fd031520d4
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2022-01-14 15:16:01 +1100

    added dplyr methods to r3 clsss

--- R/mthd_renew.R
+++ R/mthd_renew.R
@@ -11,17 +11,22 @@
 #' @param is_corresponding_lgl Is corresponding (a logical vector), Default: NA
 #' @param email_chr Email (a character vector), Default: 'NA'
 #' @param is_equal_first_lgl Is equal first (a logical vector), Default: NA
+#' @param filter_cdn_1L_chr Filter condition (a character vector of length one), Default: 'NA'
+#' @param slice_idxs_int Slice indices (an integer vector), Default: NA
 #' @return x (An object)
 #' @rdname renew-methods
 #' @export 
+#' @importFrom ready4 update_tb_r3 renew
 #' @importFrom dplyr bind_rows
 #' @importFrom tibble tibble
-#' @importFrom ready4 renew
 renew.ready4show_authors <- function (x, first_nm_chr = NA_character_, middle_nm_chr = NA_character_, 
     last_nm_chr = NA_character_, title_chr = NA_character_, qualifications_chr = NA_character_, 
     institute_chr = NA_character_, sequence_int = NA_integer_, 
-    is_corresponding_lgl = NA, email_chr = NA_character_, is_equal_first_lgl = NA) 
+    is_corresponding_lgl = NA, email_chr = NA_character_, is_equal_first_lgl = NA, 
+    filter_cdn_1L_chr = NA_character_, slice_idxs_int = NA_integer_) 
 {
+    x <- ready4::update_tb_r3(x, filter_cdn_1L_chr = filter_cdn_1L_chr, 
+        slice_idxs_int = slice_idxs_int)
     x <- dplyr::bind_rows(x, tibble::tibble(first_nm_chr = first_nm_chr, 
         middle_nm_chr = middle_nm_chr, last_nm_chr = last_nm_chr, 
         title_chr = title_chr, qualifications_chr = qualifications_chr, 
@@ -39,14 +44,19 @@ methods::setMethod("renew", methods::className("ready4show_authors", package = "
 #' @param x An instance of Name correspondences lookup table
 #' @param old_nms_chr Old names (a character vector), Default: 'NA'
 #' @param new_nms_chr New names (a character vector), Default: 'NA'
+#' @param filter_cdn_1L_chr Filter condition (a character vector of length one), Default: 'NA'
+#' @param slice_idxs_int Slice indices (an integer vector), Default: NA
 #' @return x (An object)
 #' @rdname renew-methods
 #' @export 
+#' @importFrom ready4 update_tb_r3 renew
 #' @importFrom dplyr bind_rows
 #' @importFrom tibble tibble
-#' @importFrom ready4 renew
-renew.ready4show_correspondences <- function (x, old_nms_chr = NA_character_, new_nms_chr = NA_character_) 
+renew.ready4show_correspondences <- function (x, old_nms_chr = NA_character_, new_nms_chr = NA_character_, 
+    filter_cdn_1L_chr = NA_character_, slice_idxs_int = NA_integer_) 
 {
+    x <- ready4::update_tb_r3(x, filter_cdn_1L_chr = filter_cdn_1L_chr, 
+        slice_idxs_int = slice_idxs_int)
     x <- dplyr::bind_rows(x, tibble::tibble(old_nms_chr = old_nms_chr, 
         new_nms_chr = new_nms_chr))
     return(x)
@@ -60,14 +70,19 @@ methods::setMethod("renew", methods::className("ready4show_correspondences", pac
 #' @param x An instance of ready4 S3 class for institutes lookup table
 #' @param short_name_chr Short name (a character vector), Default: 'NA'
 #' @param long_name_chr Long name (a character vector), Default: 'NA'
+#' @param filter_cdn_1L_chr Filter condition (a character vector of length one), Default: 'NA'
+#' @param slice_idxs_int Slice indices (an integer vector), Default: NA
 #' @return x (An object)
 #' @rdname renew-methods
 #' @export 
+#' @importFrom ready4 update_tb_r3 renew
 #' @importFrom dplyr bind_rows
 #' @importFrom tibble tibble
-#' @importFrom ready4 renew
-renew.ready4show_institutes <- function (x, short_name_chr = NA_character_, long_name_chr = NA_character_) 
+renew.ready4show_institutes <- function (x, short_name_chr = NA_character_, long_name_chr = NA_character_, 
+    filter_cdn_1L_chr = NA_character_, slice_idxs_int = NA_integer_) 
 {
+    x <- ready4::update_tb_r3(x, filter_cdn_1L_chr = filter_cdn_1L_chr, 
+        slice_idxs_int = slice_idxs_int)
     x <- dplyr::bind_rows(x, tibble::tibble(short_name_chr = short_name_chr, 
         long_name_chr = long_name_chr))
     return(x)
--- data-raw/mthds/renew.R
+++ data-raw/mthds/renew.R
@@ -8,7 +8,18 @@ renew.ready4show_authors <- function(x,
                                      sequence_int = NA_integer_,
                                      is_corresponding_lgl = NA,
                                      email_chr = NA_character_,
-                                     is_equal_first_lgl = NA){
+                                     is_equal_first_lgl = NA,
+                                     filter_cdn_1L_chr = NA_character_,
+                                     slice_idxs_int = NA_integer_){
+#   if(!is.na(slice_idcs_int))
+#     x <- x %>%
+#       dplyr::slice(slice_idcs_int)
+# if(!is.na(filter_cdn_1L_chr))
+#   x <- x %>%
+#     dplyr::filter(eval(parse(text=filter_cdn_1L_chr)))
+x <- ready4::update_tb_r3(x,
+                          filter_cdn_1L_chr = filter_cdn_1L_chr,
+                          slice_idxs_int = slice_idxs_int)
  x <- dplyr::bind_rows(x,
                    tibble::tibble(first_nm_chr = first_nm_chr,
                                   middle_nm_chr = middle_nm_chr,
@@ -24,7 +35,12 @@ renew.ready4show_authors <- function(x,
 }
 renew.ready4show_correspondences<- function(x,
                                             old_nms_chr = NA_character_,
-                                            new_nms_chr = NA_character_){
+                                            new_nms_chr = NA_character_,
+                                            filter_cdn_1L_chr = NA_character_,
+                                            slice_idxs_int = NA_integer_){
+  x <- ready4::update_tb_r3(x,
+                            filter_cdn_1L_chr = filter_cdn_1L_chr,
+                            slice_idxs_int = slice_idxs_int)
   x <- dplyr::bind_rows(x,
                         tibble::tibble(old_nms_chr = old_nms_chr,
                                        new_nms_chr = new_nms_chr))
@@ -32,7 +48,12 @@ renew.ready4show_correspondences<- function(x,
 }
 renew.ready4show_institutes <- function(x,
                                         short_name_chr = NA_character_,
-                                        long_name_chr = NA_character_){
+                                        long_name_chr = NA_character_,
+                                        filter_cdn_1L_chr = NA_character_,
+                                        slice_idxs_int = NA_integer_){
+  x <- ready4::update_tb_r3(x,
+                            filter_cdn_1L_chr = filter_cdn_1L_chr,
+                            slice_idxs_int = slice_idxs_int)
   x <- dplyr::bind_rows(x,
                         tibble::tibble(short_name_chr = short_name_chr,
                                        long_name_chr = long_name_chr))
