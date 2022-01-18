c8cdb13b4c853a881754d20ac24c1da1909720db
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2022-01-18 14:57:28 +1100

    redo

--- R/mthd_authorReport.R
+++ R/mthd_authorReport.R
@@ -3,15 +3,14 @@
 #' @name authorReport-Ready4showSynopsis
 #' @description authorReport method applied to Ready4showSynopsis
 #' @param x An object of class Ready4showSynopsis
-#' @param results_ls Results (a list), Default: NULL
 #' @return NULL
 #' @rdname authorReport-methods
 #' @aliases authorReport,Ready4showSynopsis-method
 #' @export 
 #' @importFrom purrr flatten_chr
+#' @importFrom ready4 make_prompt authorReport
 #' @importFrom rmarkdown render
-#' @importFrom ready4 authorReport
-methods::setMethod("authorReport", "Ready4showSynopsis", function (x, results_ls = NULL) 
+methods::setMethod("authorReport", "Ready4showSynopsis", function (x) 
 {
     if (is.na(x@fl_nm_1L_chr)) {
         fl_nm_1L_chr <- "Manuscript"
@@ -30,13 +29,17 @@ methods::setMethod("authorReport", "Ready4showSynopsis", function (x, results_ls
     write_header_fls(path_to_header_dir_1L_chr = paste0(paths_ls$path_to_ms_mkdn_dir_1L_chr, 
         "/Header"), header_yaml_args_ls = header_yaml_args_ls, 
         abstract_args_ls = x@abstract_args_ls)
-    params_ls <- list(output_type_1L_chr = x@outp_formats_chr[1], 
-        results_ls = results_ls)
-    params_ls$figures_in_body_lgl <- x@figures_in_body_lgl
-    params_ls$tables_in_body_lgl <- x@tables_in_body_lgl
-    rmarkdown::render(paste0(paths_ls$path_to_ms_mkdn_dir_1L_chr, 
-        "/Parent_", x@outp_formats_chr[1], "/Parent_", x@outp_formats_chr[1], 
-        ".Rmd"), output_format = NULL, params = params_ls, output_file = paste0(x@fl_nm_1L_chr, 
-        ifelse(x@outp_formats_chr[1] == "Word", ".docx", paste0(".", 
-            tolower(x@outp_formats_chr[1])))), output_dir = paths_ls$path_to_ms_outp_dir_1L_chr)
+    write_custom_authors(paths_ls)
+    params_ls <- list(X = x)
+    output_fl_1L_chr <- paste0(x@fl_nm_1L_chr, ifelse(x@outp_formats_chr[1] == 
+        "Word", ".docx", paste0(".", tolower(x@outp_formats_chr[1]))))
+    consent_1L_chr <- ready4::make_prompt(prompt_1L_chr = paste0("Do you confirm ('Y') that you want to write the file ", 
+        output_fl_1L_chr, " to the directory ", paths_ls$path_to_ms_outp_dir_1L_chr, 
+        " ?"), options_chr = c("Y", "N"), force_from_opts_1L_chr = T)
+    if (consent_1L_chr == "Y") {
+        rmarkdown::render(paste0(paths_ls$path_to_ms_mkdn_dir_1L_chr, 
+            "/Parent_", x@outp_formats_chr[1], "/Parent_", x@outp_formats_chr[1], 
+            ".Rmd"), output_format = NULL, params = params_ls, 
+            output_file = output_fl_1L_chr, output_dir = paths_ls$path_to_ms_outp_dir_1L_chr)
+    }
 })
--- data-raw/s4_fns/authorReport.R
+++ data-raw/s4_fns/authorReport.R
@@ -1,5 +1,4 @@
-authorReport_Ready4showSynopsis <- function (x,
-                                       results_ls = NULL) {
+authorReport_Ready4showSynopsis <- function (x) {
   if(is.na(x@fl_nm_1L_chr)){
     fl_nm_1L_chr <- "Manuscript"
   }else{
@@ -20,22 +19,37 @@ authorReport_Ready4showSynopsis <- function (x,
                                                       "/Header"),
                    header_yaml_args_ls = header_yaml_args_ls,
                    abstract_args_ls = x@abstract_args_ls)
-  params_ls <- list(output_type_1L_chr = x@outp_formats_chr[1],
-                    results_ls = results_ls)
-    params_ls$figures_in_body_lgl <- x@figures_in_body_lgl
-    params_ls$tables_in_body_lgl <- x@tables_in_body_lgl
-  # Add interactive prompt
-  rmarkdown::render(paste0(paths_ls$path_to_ms_mkdn_dir_1L_chr,
-                           "/Parent_",
-                           x@outp_formats_chr[1],
-                           "/Parent_",
-                           x@outp_formats_chr[1],
-                           ".Rmd"),
-                    output_format = NULL,
-                    params = params_ls,
-                    output_file = paste0(x@fl_nm_1L_chr,
-                                         ifelse(x@outp_formats_chr[1] == "Word",
-                                                ".docx",
-                                                paste0(".",tolower(x@outp_formats_chr[1])))),
-                    output_dir = paths_ls$path_to_ms_outp_dir_1L_chr)
+  write_custom_authors(paths_ls)
+  params_ls <- list(X = x
+    # output_type_1L_chr = x@outp_formats_chr[1],
+    #                 results_ls = results_ls
+                    )
+    # params_ls$figures_in_body_lgl <- x@figures_in_body_lgl
+    # params_ls$tables_in_body_lgl <- x@tables_in_body_lgl
+    output_fl_1L_chr <- paste0(x@fl_nm_1L_chr,
+                            ifelse(x@outp_formats_chr[1] == "Word",
+                                   ".docx",
+                                   paste0(".",
+                                          tolower(x@outp_formats_chr[1]))))
+
+  consent_1L_chr <- ready4::make_prompt(prompt_1L_chr=paste0("Do you confirm ('Y') that you want to write the file ",
+                                                             output_fl_1L_chr,
+                                                             " to the directory ",
+                                                             paths_ls$path_to_ms_outp_dir_1L_chr,
+                                                             " ?"),
+                                            options_chr = c("Y", "N"),
+                                            force_from_opts_1L_chr = T)
+
+  if(consent_1L_chr == "Y"){
+    rmarkdown::render(paste0(paths_ls$path_to_ms_mkdn_dir_1L_chr,
+                             "/Parent_",
+                             x@outp_formats_chr[1],
+                             "/Parent_",
+                             x@outp_formats_chr[1],
+                             ".Rmd"),
+                      output_format = NULL,
+                      params = params_ls,
+                      output_file = output_fl_1L_chr,
+                      output_dir = paths_ls$path_to_ms_outp_dir_1L_chr)
+    }
 }
